f9a64fe8381b87c5e196998a1a5da9f959767660
Ruhid Shukurlu <ruhids@maestro.st>
2022-11-19 23:54:47 +0400

    register view added

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="flex justify-between p-6 bg-slate-100 shadow-md">
+        <RouterLink to="/">Logo</RouterLink>
+        <nav class="space-x-2">
+            <RouterLink class="hover:text-gray-500" to="/about">About</RouterLink>
+            <RouterLink class="hover:text-gray-500" to="/register">Register</RouterLink>
+        </nav>
+    </header>
+</template>
+<script>
+export default {
+
+}
+</script>
